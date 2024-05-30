13b5c02a3cc4c1f12f6f6e6eb6804ab01744f1ff
shika <debrina.veisha@gmail.com>
2024-05-30 20:27:35 +0700

    update setup

---
+++ lama/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Fingerprint_Detection
+{
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Call the database loader to perform database operations
+            DatabaseTester.LoadDatabase();
+
+            // Show the main window
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
+
